30b1ae3e39a09962b0783818b0ba7c1556c2e7dd
Ryan Blyth <ryan.blyth@hotmail.com>
2022-05-22 18:27:16 +0000

    Cover all code in models, add more tests for views

---
+++ stock/tests/test_models.py
@@ -0,0 +1,51 @@
+"""
+Imports
+"""
+from django.test import TestCase
+from django.contrib.auth.models import User
+from stock.models import Stock, Product, Parts
+
+
+class TestModels(TestCase):
+    """Test all models"""
+    def setUp(self):
+        self.user = User.objects.create(
+            username='test_user',
+            password='12345'
+            )
+        login = self.client.login(username='testuser', password='12345')
+         
+        self.test_stock = Stock.objects.create(
+            name='test_stock',
+            code='0000',
+            number_in_stock=1,
+            company=self.user
+        )
+
+        self.test_product = Product.objects.create(
+            name='test_product',
+            company=self.user
+        )
+
+        self.test_part = Parts.objects.create(
+            product_part_belongs_to=self.test_product,
+            item=self.test_stock,
+            number_required=1,
+            company=self.user
+        )
+
+    def test_stock_return_string(self):
+        """Test stock return string"""
+        self.assertEqual(str(self.test_stock), 'test_stock | 0000 | 1')
+    
+    def test_product_return_string(self):
+        """Test product return string"""
+        self.assertEqual(str(self.test_product), 'test_product')
+    
+    def test_parts_return_string(self):
+        """Test parts return string"""
+        self.assertEqual(str(self.test_part), 'test_product test_stock 1')
+
+    def test_get_absolute_url(self):
+        """Test get_absolute_url"""
+        self.assertEqual('/', Stock.get_absolute_url(self))
--- stock/tests/test_views.py
+++ stock/tests/test_views.py
@@ -4,6 +4,7 @@ Imports
 from django.test import TestCase, Client
 from django.urls import reverse
 from django.contrib.auth.models import User
+from stock.models import Stock, Product, Parts
 
 
 class TestViews(TestCase):
@@ -14,9 +15,11 @@ class TestViews(TestCase):
         self.home_url = reverse('home')
         self.admin_view_url = reverse('admin_view')
         self.products_url = reverse('products')
+        self.stock_url = reverse('stock')
         self.add_new_product_url = reverse('add_new_product')
         self.add_new_stock_part_url = reverse('add_new_stock_part')
         self.add_parts_to_product_url = reverse('add_parts_to_product')
+        self.add_more_parts_url = reverse('add_more_parts', kwargs={'pk': 1})
         self.product_detail_url = reverse('product-detail', kwargs={'pk': 1})
 
         self.user = User.objects.create_user(
@@ -24,6 +27,25 @@ class TestViews(TestCase):
             password='12345'
             )
         login = self.client.login(username='testuser', password='12345')
+
+        self.test_stock = Stock.objects.create(
+            name='test_stock',
+            code='0000',
+            number_in_stock=100,
+            company=self.user
+        )
+
+        self.test_product = Product.objects.create(
+            name='test_product',
+            company=self.user
+        )
+
+        self.test_part = Parts.objects.create(
+            product_part_belongs_to=self.test_product,
+            item=self.test_stock,
+            number_required=5,
+            company=self.user
+        )
 
     def test_home_view(self):
         """Test home view"""
@@ -46,6 +68,13 @@ class TestViews(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'stock/products.html')
 
+    def test_stock_view(self):
+        """Test stock view"""
+        response = self.client.get(self.stock_url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'stock/stock.html')
+
     def test_add_new_product_view_get(self):
         """Test add new product GET view"""
         response = self.client.get(self.add_new_product_url)
@@ -59,3 +88,24 @@ class TestViews(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'stock/add_stock_part.html')
+    
+    def test_add_parts_to_product_view_get(self):
+        """Test add parts to product GET view"""
+        response = self.client.get(self.add_parts_to_product_url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'stock/link_parts_to_product.html')
+
+    def test_add_more_parts_get(self):
+        """Test add more parts GET view"""
+        response = self.client.get(self.add_more_parts_url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'stock/add_more_parts.html')
+
+    def test_product_detail_view(self):
+        """Test product detail view"""
+        response = self.client.get(self.product_detail_url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'stock/product_detail.html')
