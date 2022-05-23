c6aaa1bd38f9e930b01c7de56572845fd58cb8c8
Ryan Blyth <ryan.blyth@hotmail.com>
2022-05-23 20:07:24 +0000

    Fix bug not highlighting certain errors

--- static/js/errors.js
+++ static/js/errors.js
@@ -1,7 +1,7 @@
 
 let errors = document.getElementsByClassName('errorlist');
 
-for (let i = 0; i < 2; i++) {
+for (let i = 0; i <= 2; i++) {
 
     errors[i].style.backgroundColor = 'hsla(0, 58%, 86%, var(--hover))';
     errors[i].style.width = 'max-content';
